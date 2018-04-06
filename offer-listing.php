70e51e35250eab568455e3b19bf60751427a62eb
vivekkush05 <vivekkush05@gmail.com>
2018-04-06 23:53:02 +0530

    Signed-off-by: vivekkush05 <vivekkush05@gmail.com>

---
+++ offer-listing.php
@@ -0,0 +1,355 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/css/bootstrap.css" />
+<!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.css" /> // -->
+<link rel="stylesheet" type="text/css" href="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/css/style.css" />
+<link rel="stylesheet" type="text/css" href="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/css/font-awesome.min.css" />
+<link href="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/css/owl.carousel.css" rel="stylesheet">
+<!-- google fonts  --
+<link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css' />
+<link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+-- google fonts / -->
+
+
+
+<link rel="stylesheet" href="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/css/bootstrap-select.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/css/zInput_default_stylesheet.css" type="text/css" media="screen" />
+
+<script src="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/js/jquery-1.9.1.min.js"></script>
+<script>
+$(document).ready (function(){
+	
+			 $("#alert_msg").fadeTo(8000, 500).slideUp(500, function(){
+              	 $("#alert_msg").alert('close');
+              });  
+	
+ });
+</script>
+
+<style>
+.form-control { border-radius:0px; background:none;}
+
+.bootstrap-select:not([class*="col-"]):not([class*="form-control"]):not(.input-group-btn) {
+    width: 100%;
+}
+
+
+</style>
+
+
+
+</head>
+
+<body>
+
+<style>
+.nav.navbar-nav:first-child > li:first-child { display:block !important; }
+.modal-backdrop {z-index: 999;}
+</style>
+
+<div class="home-menu home-menu-bg navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top home-menu-bg">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://awn.pw/ezie-bricks/ezie-bricks/"><img src="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/images/logo/logo.png" /></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+        
+          
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"> Our Service</a>
+			
+			<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks//#1-click">Why our customers love us</a></li>
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks//#2-click">How does it works</a></li>
+ 			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks//#3-click">Facts and Figures</a></li>
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks//#4-click">How much landlords/owners can save</a></li>
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks//#5-click">Where we advertise</a></li>
+ 			<!--<li><a href="/#6-click">Success Stories</a></li>-->
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks//#7-click">How we are different</a></li>
+  			<!--<li><a href="/#8-click">Track your sale or let 24X7</a></li>
+ 			<li><a href="/#9-click">How to do youtube videos</a></li>-->						
+			</ul>						
+			</li>
+			
+			
+			
+            <li><a href="http://awn.pw/ezie-bricks/ezie-bricks/reviews">Reviews</a></li>
+            <li><a href="#">Lettings</a>
+			
+			<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks/landlords">Landlords</a></li>
+  			<li><a href="http://awn.pw/ezie-bricks/ezie-bricks/tenant-student">Tenants & Students</a></li>
+					
+			</ul>
+						
+			</li>
+            <li><a href="http://awn.pw/ezie-bricks/ezie-bricks/property-search">Property Search</a></li>
+             <li><a href="http://awn.pw/ezie-bricks/ezie-bricks/contact-us">Contact Us </a></li>
+            
+          </ul>
+		  
+        </div>
+		<!--/.nav-collapse -->
+      </div>
+    </nav>
+</div>
+
+
+
+	<!--- fixed btn --->
+		  <a><div class="banner-btn-1" data-toggle="modal" data-target="#myModal-all">FREE VALUATION</div></a>
+	  <a href="http://awn.pw/ezie-bricks/ezie-bricks/property"><div class="banner-btn-2">BOOK VIEWING</div></a>
+      
+                       
+	  				<div class="banner-btn-3"><a href="http://awn.pw/ezie-bricks/ezie-bricks/user-login">LOGIN</a></div>
+      
+      	  
+	 <!--- fixed btn /---> 
+
+
+<div class="header-height"></div>  
+    
+      <!--<div class="m-full-1">
+  
+  <div class="video-div img"><img src="assets-front/images/tenant-student/ner.jpg"></div>
+      
+      
+      
+      <div class="home-banner-data" >
+	  <div>If you have a property to sell,</div>
+       <div  class="color-yellow"> Eziebricks</div>
+	   finds Buyers and Tenants properties for FREE with No fees charged    </div>
+<div class="m-mask"></div>
+          <div id="controls-wrapper" class="load-item">
+		<div id="controls">
+			<div id="slidecaption"></div>
+			<ul id="slide-list"></ul>
+		</div>
+	</div>
+    
+   </div>-->
+    
+  
+        
+  <div class="" >
+		   
+<!-- OUR UNIQUE ONLINE PLATFORM -->		   
+<div class="container">
+<div class="row ">
+<div class="text-center margin-bottom-26">	
+
+ 
+<div class="contact-title color-5 margin-bottom" style="margin:0px;">OFFER LIST</div>
+
+</div>
+</div>
+</div>
+
+
+
+
+<!-- listing -->
+<div class="container">
+<div class="row">
+
+
+
+<!-- RE -->
+<a href="#" class="color-7" target="_blank">
+<div class="list-group border relative" style="padding:15px 0px;">
+<div class="list-group-item">
+<div class="media col-md-3">
+<img src="http://awn.pw/ezie-bricks/ezie-bricks/timthumb.php?src=http://awn.pw/ezie-bricks/ezie-bricks/uploads/propertyimg/1aaa877d-6f52-4515-b46d-cd4a78a5295d-1470206539.jpg&w=246&amp;h=160&amp;zc=1&amp;f=0|0|0|0|0"/>
+</div>
+<div class="col-md-9">
+<div class="contact-title color-5 margin-bottom">Offer Name</div>
+<p class="color-7">User name</p>
+<div class="price-img"><div class="price-img-inr"><i class="fa fa-inr" aria-hidden="true"></i> 234456 </div> </div>
+
+</div>
+<div class="clearfix"></div>
+
+<div class="text-right" style="position:absolute; right:10px; bottom:10px; ">
+<a href="#" class = "btn btn-danger color-3">Reject</a> &nbsp;
+<a href="#" class = "btn btn-success color-3">Accept</a>
+<!--<button type = "button" class = "btn background-2 color-3">Book Viewing</button>-->
+</div>
+</div>
+</div>
+</a>
+<!-- RE /-->
+
+
+<!-- RE -->
+<a href="#" class="color-7" target="_blank">
+<div class="list-group border relative" style="padding:15px 0px;">
+<div class="list-group-item">
+<div class="media col-md-3">
+<img src="http://awn.pw/ezie-bricks/ezie-bricks/timthumb.php?src=http://awn.pw/ezie-bricks/ezie-bricks/uploads/propertyimg/1aaa877d-6f52-4515-b46d-cd4a78a5295d-1470206539.jpg&w=246&amp;h=160&amp;zc=1&amp;f=0|0|0|0|0"/>
+</div>
+<div class="col-md-9">
+<div class="contact-title color-5 margin-bottom">Offer Name</div>
+<p class="color-7">User name</p>
+<div class="price-img"><div class="price-img-inr"><i class="fa fa-inr" aria-hidden="true"></i> 234456 </div> </div>
+
+</div>
+<div class="clearfix"></div>
+
+<div class="text-right" style="position:absolute; right:10px; bottom:10px; ">
+<a href="#" class = "btn btn-danger color-3">Reject</a> &nbsp;
+<a href="#" class = "btn btn-success color-3">Accept</a>
+<!--<button type = "button" class = "btn background-2 color-3">Book Viewing</button>-->
+</div>
+</div>
+</div>
+</a>
+<!-- RE /-->
+
+
+<!-- RE -->
+<a href="#" class="color-7" target="_blank">
+<div class="list-group border relative" style="padding:15px 0px;">
+<div class="list-group-item">
+<div class="media col-md-3">
+<img src="http://awn.pw/ezie-bricks/ezie-bricks/timthumb.php?src=http://awn.pw/ezie-bricks/ezie-bricks/uploads/propertyimg/1aaa877d-6f52-4515-b46d-cd4a78a5295d-1470206539.jpg&w=246&amp;h=160&amp;zc=1&amp;f=0|0|0|0|0"/>
+</div>
+<div class="col-md-9">
+<div class="contact-title color-5 margin-bottom">Offer Name</div>
+<p class="color-7">User name</p>
+<div class="price-img"><div class="price-img-inr"><i class="fa fa-inr" aria-hidden="true"></i> 234456 </div> </div>
+
+</div>
+<div class="clearfix"></div>
+
+<div class="text-right" style="position:absolute; right:10px; bottom:10px; ">
+<a href="#" class = "btn btn-danger color-3">Reject</a> &nbsp;
+<a href="#" class = "btn btn-success color-3">Accept</a>
+<!--<button type = "button" class = "btn background-2 color-3">Book Viewing</button>-->
+</div>
+</div>
+</div>
+</a>
+<!-- RE /-->
+
+
+
+</div>
+
+
+
+</div>
+<!-- listing /--->
+
+
+
+
+
+
+
+
+
+   
+
+   
+<script src="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/js/bootstrap.min.js"></script>
+<script src="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/js/bootstrap-select.min.js"></script> 
+  
+
+   <footer>
+   
+    <div class="container">
+    <div class="row">
+    
+     <div class="col-sm-4">
+      <div class="footer-title"><img src="http://awn.pw/ezie-bricks/ezie-bricks/assets-front/images/logo/footer-logo.png" /></div>
+      <div class="footer-title-line"></div>
+      
+      <ul>
+       <li><a href="#">Lorem ipsum aspernatur aut odit aut fugit, sed elits quias consequuntur magni dolores eos qui ratione 
+volust luptatem sequi nesciunt.</a> </li>
+
+
+       <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> 10th Floor,Washington Square Park, NY, United States. </a> </li>
+       <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i> (004)+ 124 45 78 678 </a> </li>
+       <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i> Support@domain.com</a> </li>
+
+       
+      </ul>
+     
+     </div>
+     
+     
+     <div class="col-sm-4">
+      <div class="footer-title">Quick Links</div>
+      <div class="footer-title-line"></div>
+            <ul>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> TENANCY AGREEMENTS </a></li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> HELPFUL GUIDES</a> </li>
+       <li><a href="tenant-student.php"><i class="fa fa-caret-right" aria-hidden="true"></i> TENANT FIND</a> </li>
+       <li><a href="term&condition.php"><i class="fa fa-caret-right" aria-hidden="true"></i> TERM & CONDITION</a> </li>
+       <li><a href="blog.php"><i class="fa fa-caret-right" aria-hidden="true"></i> BLOG</a> </li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> NEW</a> </li>
+       <li><a href="term&condition.php"><i class="fa fa-caret-right" aria-hidden="true"></i> PRIVACY POLICY</a> </li>
+       
+      </ul>
+     
+     </div>
+     
+     
+     
+     
+     <div class="col-sm-4">
+      <div class="footer-title">career Opportunities</div>
+      <div class="footer-title-line"></div>
+      
+      <ul>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> TENANCY AGREEMENTS </a></li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> HELPFUL GUIDES</a> </li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> TENANT FIND</a> </li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> TERM & CONDITION</a> </li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> BLOG</a> </li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> NEW</a> </li>
+       <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i> PRIVACY POLICY</a> </li>
+      
+       
+      </ul>
+     
+     </div>
+    
+    
+    </div>
+	
+	<!--<div class="my-divider text-center img"><img src="assets/images/img-1/devider-footer.png"></div>-->
+	
+	
+	<div class="social text-center padding-30">
+    <ul>
+        <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+
+    </ul>
+<p>@ EZIY BRICKS | ALL RIGHTS RESERVED</p>	
+</div>
+   </div>
+   
+
+   </footer>
+
+   </div>
+  </body>
+</html>
